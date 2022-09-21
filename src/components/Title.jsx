1e57be0dcb0a144c97d71364014b368dc14802d7
DanielD0 <37585619+DanielD0@users.noreply.github.com>
2022-09-21 12:21:20 -0500

    first commit

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div className='liquid'>
+        <h2>Daniel</h2>
+        <h2>Daniel</h2>
+        <h2>Daniel</h2>
+        <h2>Daniel</h2>
+    </div>
+  )
+}
+
+export default Title
